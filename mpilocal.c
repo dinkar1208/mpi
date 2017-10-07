4164af5c089c72794930f47ec9b15be9f133ed04
DINU <dkhand3@uic.edu>
2017-10-07 08:46:53 -0500

    Scatter and reduce

--- mpilocal.c
+++ mpilocal.c
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <assert.h>
 
-#define NUM_MAX 1000000
+#define NUM_MAX 100
 
 int main(int argc, char** argv) {
   /*** Initialize the MPI environment ***/
@@ -19,11 +19,11 @@ int main(int argc, char** argv) {
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
   /*** Creating new virtual topology ***/ 
-  int dims = world_size;
-  int qperiodic= 1;
-  int reorder = 1;
-  MPI_Dims_create(world_size, 1, &dims);
-  MPI_Cart_create(MPI_COMM_WORLD, 1, &dims, &qperiodic, reorder, &newcomm);
+  const int dims[] = {2,2,2};
+  const int qperiodic[2] = {1,1,1};
+  const int reorder = 1;
+//  MPI_Dims_create(world_size, 2, &dims);
+  MPI_Cart_create(MPI_COMM_WORLD, 3, &dims, &qperiodic, reorder, &newcomm);
   /*** Getting new world rank ***/
   MPI_Comm_rank(newcomm, &world_rank);
 
@@ -34,11 +34,13 @@ int main(int argc, char** argv) {
   float *numbers;
   float *buf;
 
+
   printf("World size is %d and world rank is %d\n\n", world_size, world_rank);
   srand(time(0));
   /*** Broadcast and receive ***/
   numbers = (float *)malloc(sizeof(float) * NUM_MAX);
-  buf = (float *)malloc(sizeof(float) * NUM_MAX);
+  int split = (sizeof(numbers)/sizeof(numbers[0]))/world_size;
+  buf = (float *)malloc(sizeof(float) * split);
   if(world_rank == 0) {
     // If we are rank 0, set the number to -1 and send it to process 1
 
@@ -47,24 +49,19 @@ int main(int argc, char** argv) {
 	numbers[i] = (rand() / (float)RAND_MAX)*10;
 	//printf("  %d,", numbers[i]);
     }
-	MPI_Bcast(numbers, NUM_MAX, MPI_INT, 0, newcomm);
   }
-  else {
 
-	MPI_Bcast(buf, NUM_MAX, MPI_INT ,0 , newcomm);
-  }
- // printf("Broadcast successful\n\r");
+  MPI_Scatter(numbers, split, MPI_FLOAT, buf, split, MPI_FLOAT, 0, MPI_COMM_WORLD);
+  printf("Scatter successful\n\r");
 
   /*** Summing in individal processes ***/
   if(world_rank != 0)
   {
-	int from = (world_rank - 1)*(NUM_MAX/(world_size - 1));
-	int to = world_rank * (NUM_MAX/(world_size - 1));
-	for(i = from; i < to; i++)
+	for(i = 0; i < split; i++)
 	{
 		sum = sum + buf[i];
 	}
-	printf("Sum from %d is %f  -> (%d,%d)\n", world_rank, sum,from,to);
+	printf("Sum from %d is %f\n", world_rank, sum);
   }
  	MPI_Reduce(&sum, &total, 1, MPI_FLOAT, MPI_SUM, 0, newcomm);
 
@@ -80,6 +77,7 @@ int main(int argc, char** argv) {
   
   free(numbers);
   free(buf);
+  MPI_Comm_free(&newcomm);
   MPI_Finalize();
 }
 
